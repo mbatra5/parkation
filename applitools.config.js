a9734b6ce99126997bbdf914242668991d2f8549
Madhur.Batra <bamzny@bp365.bp.com>
2022-12-10 15:27:19 +0530

    Parkation 1.0 Created

---
+++ applitools.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testConcurrency: 20,
+  apiKey: "BeU1Z102o104fV107HOTvz1gxcAIXZ2Wo8Z4QmcuB102cPmXQLM110",
+  failCypressOnDiff: false,
+  ignoreDisplacements: true,
+  notifyOnCompletion: true,
+  serverUrl: "https://bpeyesapi.applitools.com/"
+};
